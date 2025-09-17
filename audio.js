fafe8708ec48688152a33bfa85d81ae10da1405a
hi <115565725+Mikeblackbelt@users.noreply.github.com>
2025-09-16 20:45:59 -0400

    whoops
    i forgot a parenthesis

--- audio.js
+++ audio.js
@@ -5,3 +5,4 @@ function playSound(url) {
 
 document.addEventListener('DOMContentLoaded', () => {
     playSound('https://say-yes-if-you-are-cool.netlify.app/11l-fail_sound_comedy_r-1749704575880-358768.mp3')
+});
