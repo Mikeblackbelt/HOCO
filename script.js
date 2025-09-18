6b3bc501392f2169704c9d4c766f861080ec9cac
hi <115565725+Mikeblackbelt@users.noreply.github.com>
2025-09-18 13:44:29 -0400

    they said yes
    but they didnt notice the confetti
    you win some you lose some

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 let particles = [];
-const particleCount = 30;
+const particleCount = 50;
 
 canvas.style.position = 'absolute';
 canvas.style.display = 'none';
